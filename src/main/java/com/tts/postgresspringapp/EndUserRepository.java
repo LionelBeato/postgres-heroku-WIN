aba27dc68f7a95485de0b6be4a6866a81ec39e26
Lionel Beato <lionel.beato@gmail.com>
2020-08-13 08:25:04 -0400

    first commit

---
+++ src/main/java/com/tts/postgresspringapp/EndUserRepository.java
@@ -0,0 +1,9 @@
+package com.tts.postgresspringapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EndUserRepository extends JpaRepository<EndUser, Long> {
+
+    
+    
+}
